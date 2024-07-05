abcbb9a6753d3aa0f7f32a69c52a0fe93703265a
00003688 <kadiraydogdu@tcdd.gov.tr>
2024-07-05 22:22:22 +0300

    first commit

---
+++ ChatApp.Data/Entities/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatApp.Data;
+
+public class Message: BaseEntity
+{
+    public string Content { get; set;}
+    public int? UserId { get; set; }
+    public User User{ get; set; }
+    public int? GroupId { get; set; }
+    public Group Group{ get; set; }
+}
+
