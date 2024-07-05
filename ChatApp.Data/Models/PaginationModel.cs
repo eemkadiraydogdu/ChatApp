5fb979052665530f0220d0bb7bf52d35d61f78c4
00003688 <kadiraydogdu@tcdd.gov.tr>
2024-07-05 22:55:11 +0300

    Repository ler oluşturuldu

---
+++ ChatApp.Data/Models/PaginationModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ChatApp.Data;
+
+public class PaginationModel
+{
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+}
--- ChatApp.Data/obj/Debug/net8.0/ChatApp.Data.AssemblyInfo.cs
+++ ChatApp.Data/obj/Debug/net8.0/ChatApp.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApp.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abcbb9a6753d3aa0f7f32a69c52a0fe93703265a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApp.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApp.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
